9ff270432fce85815c34bd03e39a03db46ac091f
Austin Wang <austin.wang0311@gmail.com>
2017-01-16 20:11:58 -0500

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public Fractal fractalPrefab;
+
+	private Fractal fractalInstance;
+
+	// Use this for initialization
+	void Start () {
+		BeginGame ();
+	}
+		
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space)) {
+			RestartGame ();
+		}
+	}
+
+	private void BeginGame() {
+		fractalInstance = Instantiate (fractalPrefab) as Fractal;
+	}
+
+	private void RestartGame() {
+		StopAllCoroutines ();
+		Destroy (fractalInstance.gameObject);
+		BeginGame ();
+	}
+}
